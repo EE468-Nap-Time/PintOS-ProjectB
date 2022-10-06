33cd4e0f6d36d502212d9c438c9e444058370948
ethancheez <ethancheez@gmail.com>
2022-10-06 01:50:28 -1000

    Added functions in syscall.c

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,10 +1,11 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
-#include <sys/types.h>
+#include <user/syscall.h>
 #include "stdbool.h"
 
 void syscall_init (void);
+void syscall_halt(void);
 void syscall_exit(int status);
 pid_t syscall_exec(const char *cmd_line);
 int syscall_wait(pid_t pid);
