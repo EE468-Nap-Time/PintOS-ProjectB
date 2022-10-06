a693b8381d91e8be0a6792cf662c36713fe07fec
ethancheez <ethancheez@gmail.com>
2022-10-06 00:20:08 -1000

    Defined syscall functions in header

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,15 +6,15 @@
 
 static void syscall_handler (struct intr_frame *);
 
-void
-syscall_init (void) 
-{
+void syscall_init (void) {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
-{
+static void syscall_handler (struct intr_frame *f UNUSED)  {
   printf ("system call!\n");
+
+  // Get stack pointer
+  void *esp = f->esp;
+
   thread_exit ();
 }
