7d2bea819dbfb4225ca8f0188a842342367822c5
ethancheez <ethancheez@gmail.com>
2022-10-06 00:26:01 -1000

    Fix function header typos/data type

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,16 +1,18 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <sys/types.h>
+
 void syscall_init (void);
 void syscall_exit(int status);
 pid_t syscall_exec(const char *cmd_line);
 int syscall_wait(pid_t pid);
-bool syscall_create(const char *file, unsigned intitlal_size);
-bool syscall_remove(const char *file);
+int syscall_create(const char *file, unsigned intitlal_size);
+int syscall_remove(const char *file);
 int syscall_open(const char *file);
 int syscall_filesize(int fd);
 int syscall_read(int fd, void *buffer, unsigned size);
-int syscall_write(int fd, const void *buffer unsigned size);
+int syscall_write(int fd, const void *buffer, unsigned size);
 void syscall_seek(int fd, unsigned position);
 unsigned syscall_tell(int fd);
 void syscall_close(int fd);
